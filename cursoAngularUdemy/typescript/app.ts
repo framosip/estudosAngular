e104fb0aa346bf6cb1d4a8bd3f79372b090255e4
Fernando Ramos <framosip@gmail.com>
2019-08-20 22:32:38 -0300

    TypeScript

---
+++ cursoAngularUdemy/typescript/app.ts
@@ -0,0 +1,11 @@
+let message: string = "Help me, Obi-Wan Kenobi. You're my only hope"
+console.log(message)
+
+let episode: number = 4
+console.log("This is episode " + 4)
+episode = episode + 1
+console.log("Next epidose is " + episode)
+
+let favoriteDroid //Por padrão quando um tipo de variável não é informada, ela é do tipo 'Any'
+favoriteDroid = 'BB-8'
+console.log("My favorite droid is " + favoriteDroid)
---
+++ cursoAngularUdemy/typescript/dist/app.js
@@ -0,0 +1,9 @@
+var message = "Help me, Obi-Wan Kenobi. You're my only hope";
+console.log(message);
+var episode = 4;
+console.log("This is episode " + 4);
+episode = episode + 1;
+console.log("Next epidose is " + episode);
+var favoriteDroid; //Por padrão quando um tipo de variável não é informada, ela é do tipo 'Any'
+favoriteDroid = 'BB-8';
+console.log("My favorite droid is " + favoriteDroid);
