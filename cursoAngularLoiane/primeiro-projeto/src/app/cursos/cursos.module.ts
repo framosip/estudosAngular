1e75c137f40e7913331dfdedb0873001681eb346
Fernando Ramos <framosip@gmail.com>
2019-09-23 22:39:03 -0300

    Introdução a serviços e injeção de dependência

--- cursoAngularLoiane/primeiro-projeto/src/app/cursos/cursos.module.ts
+++ cursoAngularLoiane/primeiro-projeto/src/app/cursos/cursos.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CursosComponent } from './cursos.component';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { CursosService } from './cursos.service';
 
 @NgModule({
   declarations: [CursosComponent, CursoDetalheComponent],
@@ -10,6 +11,9 @@ import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
   ],
   exports: [ //Informa quais componentes estão exportados para outros módulos
     CursosComponent
+  ],
+  providers: [ //Informa os serviços fornecedores
+    CursosService
   ]
 })
 export class CursosModule { }
